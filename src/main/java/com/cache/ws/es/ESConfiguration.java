089b5077e2abc4d01ff1abcd80146d8cb4135b0f
TomDing729 <327523994@qq.com>
2015-07-17 17:58:34 +0800

    集成jest.

---
+++ src/main/java/com/cache/ws/es/ESConfiguration.java
@@ -0,0 +1,80 @@
+package com.cache.ws.es;
+
+import io.searchbox.client.JestClient;
+import io.searchbox.client.JestClientFactory;
+import io.searchbox.client.JestResult;
+import io.searchbox.client.config.HttpClientConfig;
+import io.searchbox.core.Search;
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.common.settings.Settings;
+
+public class ESConfiguration {
+
+	private static ESConfiguration singleton = new ESConfiguration();
+
+	@SuppressWarnings("unused")
+	private JestClient client;
+
+	private Settings settings;
+
+	private ESConfiguration() {
+		settings = ImmutableSettings.settingsBuilder()
+				.loadFromClasspath("cache.yml").build();
+
+	}
+
+	public static ESConfiguration getInstance() {
+		return singleton;
+	}
+
+	public Settings getSettings() {
+		return settings;
+	}
+
+	public JestClient getClient() {
+		String connectionUrl = "http://" + settings.get("ip") + ":"
+				+ settings.get("port");
+
+		JestClientFactory factory = new JestClientFactory();
+
+		factory.setHttpClientConfig(new HttpClientConfig.Builder(connectionUrl)
+				.multiThreaded(true).build());
+		JestClient client = factory.getObject();
+		return client;
+	}
+
+	public void setClient(JestClient client) {
+		this.client = client;
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		JestClient client = ESConfiguration.getInstance().getClient();
+
+		String query = "{\n" + "    \"query\": {\n"
+				+ "        \"filtered\" : {\n" + "            \"query\" : {\n"
+				+ "                \"query_string\" : {\n"
+				+ "                    \"query\" : \"宣城市\"\n"
+				+ "                }\n" + "            }\n" + "        }\n"
+				+ "    }\n" + "}";
+
+		Search search = (Search) new Search.Builder(query)
+		// multiple index or types can be added.
+				.addIndex("access-2015-07-16").addType("1").build();
+
+		JestResult result = client.execute(search);
+
+		// SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+		// searchSourceBuilder.query(QueryBuilders.queryStringQuery("宣城市"));
+		//
+		// Search search = (Search) new Search.Builder(
+		// searchSourceBuilder.toString())
+		// .addIndex("access-2015-07-16").addType("1").build();
+		//
+		// JestResult result = client.execute(search);
+
+		System.out.print(result.getJsonString());
+
+	}
+
+}
--- src/main/java/com/cache/ws/rest/MessageRestService.java
+++ src/main/java/com/cache/ws/rest/MessageRestService.java
@@ -1,24 +1,34 @@
-package com.cache.ws.rest;  
-  
-import javax.ws.rs.GET;  
-import javax.ws.rs.Path;  
-import javax.ws.rs.PathParam;  
-  
-  
-import javax.ws.rs.core.Response;  
-   
-@Path("/message")  
-public class MessageRestService {  
-   public MessageRestService(){} 
-    @GET 
-    @Path("/{param}")  
-    public Response printMessage(@PathParam("param") String msg) {  
-   
-        String result = "Restful example : " + msg;  
-   
-        return Response.status(200).entity(result).build();  
-   
-    }  
-   
-  
-}  
+package com.cache.ws.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+import javax.ws.rs.core.Response;
+
+@Path("/message")
+public class MessageRestService {
+	public MessageRestService() {
+	}
+
+	@GET
+	@Path("/{ }")
+	public Response printMessage(@PathParam("param") String msg) {
+
+		String result = "Restful example : " + msg;
+
+		return Response.status(200).entity(result).build();
+
+	}
+
+	@GET
+	@Path("/get/{param}")
+	public Response getMessage(@PathParam("param") String msg) {
+
+		String result = "Restful example12321321 : " + msg;
+
+		return Response.status(200).entity(result).build();
+
+	}
+
+}
