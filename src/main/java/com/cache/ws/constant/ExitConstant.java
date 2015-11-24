56227a041e77ba8ee2ab7a10c19de2f430c4efe5
TomDing729 <327523994@qq.com>
2015-11-24 15:26:45 +0800

    修改配置文件

--- src/main/java/com/cache/ws/constant/ExitConstant.java
+++ src/main/java/com/cache/ws/constant/ExitConstant.java
@@ -2,7 +2,7 @@ package com.cache.ws.constant;
 
 public class ExitConstant {
 	
-	public static String MONGODB_NAME_EXIT = "exit-indicator";
+	public static String MONGODB_NAME_EXIT = "exit-indicator-";
 
 	public static String DB_NAME = "exit";
 }
